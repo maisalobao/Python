4aa1f867688289ef61f40ea2dfd275c22e86b364
Maísa Lobão <maisalob@hotmail.com>
2022-03-17 11:08:19 -0300

    Create desafio089.py

---
+++ pythonProject/desafio089.py
@@ -0,0 +1,24 @@
+lista = list()
+while True:
+    nome = str(input('Nome: '))
+    n1 = float(input('Nota 1:'))
+    n2 = float(input('Nota 2:'))
+    media = (n1 + n2) / 2
+    lista.append([nome, [n1, n2], media])
+    escolha = str(input('Deseja continuar? [S/N]')).strip().upper()[0]
+    if escolha in 'Nn':
+        break
+print('-' * 30)
+print(f'{"Nº":<4}{"NOME":<10}{"MEDIA":<7}')
+print('-' * 30)
+for i, v in enumerate(lista):
+    print(f'{i:<4}{v[0]:<10}{v[2]:>6}')
+print('-' * 30)
+while True:
+    notas = int(input('Deseja ver a nota de qual aluno? (999 interrompe)'))
+    if notas == 999:
+        break
+    for i,v in enumerate(lista):
+        if i == notas:
+            print(f'As notas de {v[0]} são {v[1]}.')
+print('VOLTE SEMPRE')
