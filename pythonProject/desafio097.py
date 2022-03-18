81970dcd2d10cb96d6c61279f9369c7efaee6358
Maísa Lobão <maisalob@hotmail.com>
2022-03-18 07:06:54 -0300

    Create desafio097.py

---
+++ pythonProject/desafio097.py
@@ -0,0 +1,10 @@
+def escreva(esc):
+    print('-' * (len(esc)+3))
+    print(esc)
+    print('-' * (len(esc)+3))
+
+
+#Programa principal
+escreva('Maísa Lobão')
+escreva('É muito lindaaa')
+escreva('Amo Python')
