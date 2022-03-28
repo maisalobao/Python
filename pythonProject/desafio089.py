aa73b81f650edeefe8360e2d4ee09dde903afe1a
Maísa Lobão <99517766+maisalobao@users.noreply.github.com>
2022-03-27 22:57:53 -0300

    Update desafio089.py

--- pythonProject/desafio089.py
+++ pythonProject/desafio089.py
@@ -1,3 +1,6 @@
+#Desafio: Crie um programa que leia nome e duas notas de vários alunos e guarde tudo em uma lista composta. 
+#No final, mostre um boletim contendo a média de cada um e permita que o usuário possa mostrar as notas de cada aluno individualmente.
+
 lista = list()
 while True:
     nome = str(input('Nome: '))
