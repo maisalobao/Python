8b8fa9796853c8f094cb1602e7e45f57905a9057
Maísa Lobão <99517766+maisalobao@users.noreply.github.com>
2022-04-07 20:38:29 -0300

    Update desafio057.py

--- pythonProject/desafio057.py
+++ pythonProject/desafio057.py
@@ -1,3 +1,6 @@
+#Desafio: Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. 
+#Caso esteja errado, peça a digitação novamente até ter um valor correto.
+
 sexo = ''
 while sexo != 'M' and sexo != 'F':
     sexo = str(input('Sexo:')).upper()
