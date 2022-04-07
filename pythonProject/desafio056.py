5111e6f2b91aff9fb6732590e866e06f8688532f
Maísa Lobão <99517766+maisalobao@users.noreply.github.com>
2022-04-07 20:37:09 -0300

    Update desafio056.py

--- pythonProject/desafio056.py
+++ pythonProject/desafio056.py
@@ -1,3 +1,6 @@
+#Desafio: desenvolva um programa que leia o nome, idade e sexo de 4 pessoas. 
+#No final do programa, mostre: a média de idade do grupo, qual é o nome do homem mais velho e quantas mulheres têm menos de 20 anos.
+
 lista_idade = []
 maior_idade_homem = 0
 nome_velho = ''
